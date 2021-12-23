ac51dbb2af2edf4bc40cfb28f890d7ebb4b34739
widjoyono <abrorabyyu9@gmail.com>
2021-12-23 20:21:41 +0800

    init

---
+++ algo/easy/sortedSquaredArray.py
@@ -0,0 +1,36 @@
+"""
+
+def sortedSquaredArray(array):
+    sortedSquares = [0 for _ in array]
+
+    for idx in range(len(array)):
+        value = array[idx]
+        sortedSquares[idx] = value * value
+
+        sortedSquares.sort()
+        return sortedSquares
+"""
+
+
+def sortedSquaredArray(array):
+    sortedSquares = [0 for _ in array]
+    smallerValueIdx = 0
+    largerValueIdx = len(array) - 1
+
+    for idx in reversed(range(len(array))):
+        smallerValue = array[smallerValueIdx]
+        largerValue = array[largerValueIdx]
+
+        if abs(smallerValue) > abs(largerValue):
+            sortedSquares[idx] = smallerValue * smallerValue
+            smallerValueIdx += 1
+
+        else:
+            sortedSquares[idx] = largerValue * largerValue
+            largerValueIdx -= 1
+
+    return sortedSquares
+
+
+if __name__ == '__main__':
+    print(sortedSquaredArray([-3,-1,2,5,10]))
